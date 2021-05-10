f7e8bce07497a63ae335371558d23c94fad50992
Pikkel <76259034+Pikkel@users.noreply.github.com>
2021-05-10 09:44:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-<!DOCTYPE html>
-<html>
-    <head><title>c</title></head>
-    <body>
-        Nice IP, did your ISP choose it for you? < ? php echo $_SERVER['REMOTE_ADDR'] ? >
-    </body>
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
     <meta content="Pogland" property="og:title">
     <meta content="https://embed.com/pogland.club" property="og:url">
     <meta content="#7642a5" data-react-helmet="true" name="theme-color">
-</html>
