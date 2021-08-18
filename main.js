5e627dc208d5de06e3b049be3585ca798ba52afe
Pikkel <76259034+Pikkel@users.noreply.github.com>
2021-08-17 23:03:40 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,67 @@
+let counter = 0;
+
+const params = new URLSearchParams(window.location.search);
+
+$(document).ready(_ => {
+  if (params.get("url") != null) {
+    $("input")[0].value = params.get("url");
+  }
+  if (params.get("msg") != null) {
+    $("input")[1].value = params.get("msg");
+  }
+});
+
+let save = _ => {
+  window.location.search = "?msg=" + $("input")[1].value + "&url=" + $("input")[0].value;
+}
+
+let url;
+let msg;
+let rate;
+let cooldown;
+
+let submit = _ => {
+  url = $("input")[0].value;
+  msg = $("input")[1].value;
+  rate = $("input")[2].value;
+  cooldown = $("input")[3].value;
+
+  log("SYS", "Attempting to send '" + msg + "' to '" + url + "' every " + rate + "ms")
+  send();
+  document.body.classList.toggle("h");
+}
+
+let send = _ => {
+  $("button")[1].disabled = true;
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    async: true,
+    data: { content: msg },
+    complete: e => {
+      if (e.status == 204) {
+        counter++;
+        log(e.status, "Webhook Spammer | Sent " + counter.toLocaleString() + " messages");
+        setTimeout(send, rate);
+      } else if (e.status == 429) {
+        log(e.status, "Webhook Spammer | You are being rate-limited");
+        log("SYS", "Taking a " + cooldown + "ms break...");
+        setTimeout(send, cooldown);
+      } else {
+        if (e.status == 404) log(e.status, "Webhook not found !");
+        if (e.status == 0) log(e.status, "AJAX request failed");
+        log("SYS", "Reattempting...");
+        setTimeout(send, rate);
+      }
+    }
+  });
+}
+
+let logtext = [];
+let log = (code, log) => {
+  let day = new Date().toString().split(" ")[4];
+  logtext.unshift("<span>" + day + "</span> -- [" + code + "] " + log);
+  if (logtext.length > 50) logtext.pop();
+  $("code").html(logtext.join("<br>"));
+}
